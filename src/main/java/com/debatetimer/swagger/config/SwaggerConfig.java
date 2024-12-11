d4bc30230348c9191d5a4b5cb61f966521ab0154
coli-geonwoo <coli-geonwoo@gmail.com>
2024-12-12 01:52:58 +0900

    style: 불필요한 import 삭제

--- src/main/java/com/debatetimer/swagger/config/SwaggerConfig.java
+++ src/main/java/com/debatetimer/swagger/config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package com.debatetimer.swagger.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
