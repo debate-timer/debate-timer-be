f33131fa333c429731dd0fd38357ab30f28990f6
unifolio0 <boho82ha@gmail.com>
2024-12-17 15:32:45 +0900

    test: 불필요한 롤백 기능 제거

--- src/test/java/com/debatetimer/BaseRepositoryTest.java
+++ src/test/java/com/debatetimer/BaseRepositoryTest.java
@@ -1,10 +1,7 @@
 package com.debatetimer;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-
-@ExtendWith(DataBaseCleaner.class)
 @DataJpaTest
 public abstract class BaseRepositoryTest {
 }
