5e17d02f499ad1c5150d8b164c2e3bbbd042762a
unifolio0 <boho82ha@gmail.com>
2024-12-22 12:49:28 +0900

    test: MemberTest

---
+++ src/test/java/com/debatetimer/domain/MemberTest.java
@@ -0,0 +1,29 @@
+package com.debatetimer.domain;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberTest {
+    @Nested
+    class Validate {
+
+        @ParameterizedTest
+        @ValueSource(ints = {0, 11})
+        void 닉네임은_1자_이상_20자_이하여야_한다(int length) {
+            assertThatThrownBy(() -> new Member("f".repeat(length)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("닉네임은 1자 이상 10자 이하여야 합니다");
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"abc12", "가나다12"})
+        void 닉네임은_영문과_한글만_가능하다(String nickname) {
+            assertThatThrownBy(() -> new Member(nickname))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("닉네임은 영문/한글만 가능합니다");
+        }
+    }
+}
