280135f80f22dfc45261bce7a6f63af8f3cf96e7
coli-geonwoo <coli-geonwoo@gmail.com>
2024-12-23 02:20:51 +0900

    refactor: 핸들링 에러 추가

--- src/main/java/com/debatetimer/controller/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/debatetimer/controller/exception/handler/GlobalExceptionHandler.java
@@ -3,18 +3,24 @@ package com.debatetimer.controller.exception.handler;
 import com.debatetimer.controller.exception.ErrorResponse;
 import com.debatetimer.controller.exception.custom.DTClientErrorException;
 import com.debatetimer.controller.exception.custom.DTServerErrorException;
+import jakarta.validation.ConstraintViolationException;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.ClientAbortException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -25,6 +31,45 @@ public class GlobalExceptionHandler {
                 .collect(Collectors.joining(" | "));
         log.warn("message: {}", exceptionMessage);
         return toResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindingException(BindException exception) {
+        log.warn("message: {}", exception.getMessage());
+        return toResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException exception) {
+        log.warn("message: {}", exception.getMessage());
+        return toResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException exception) {
+        log.warn("message: {}", exception.getMessage());
+        return toResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(ClientAbortException.class)
+    public ResponseEntity<ErrorResponse> handleClientAbortException(ClientAbortException exception) {
+        log.warn("message: {}", exception.getMessage());
+        return toResponse(HttpStatus.BAD_REQUEST, "");
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException exception) {
+        log.warn("message: {}", exception.getMessage());
+        return toResponse(HttpStatus.METHOD_NOT_ALLOWED, "");
+    }
+
+    @ExceptionHandler()
+    public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(
+            HttpMediaTypeNotSupportedException exception) {
+        log.warn("message: {}", exception.getMessage());
+        return toResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "");
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
