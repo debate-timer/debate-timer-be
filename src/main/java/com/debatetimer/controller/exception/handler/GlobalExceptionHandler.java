a41fa9dd6f41557ac73c49e52e1eeca537c29139
coli-geonwoo <coli-geonwoo@gmail.com>
2024-12-23 01:40:10 +0900

    refactor: 에러 핸들링 세분화

--- src/main/java/com/debatetimer/controller/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/debatetimer/controller/exception/handler/GlobalExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.debatetimer.controller.exception.handler;
 
 import com.debatetimer.controller.exception.ErrorResponse;
-import com.debatetimer.controller.exception.custom.DTException;
+import com.debatetimer.controller.exception.custom.DTClientErrorException;
+import com.debatetimer.controller.exception.custom.DTServerErrorException;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,7 +18,8 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
         String exceptionMessage = exception.getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(" | "));
@@ -31,16 +32,22 @@ public class GlobalExceptionHandler {
         return toResponse(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 
-    @ExceptionHandler(DTException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(DTException exception) {
+    @ExceptionHandler(DTClientErrorException.class)
+    public ResponseEntity<ErrorResponse> handleClientException(DTServerErrorException exception) {
+        log.warn("message: {}", exception.getMessage());
+        return toResponse(exception.getHttpStatus(), exception.getMessage());
+    }
+
+    @ExceptionHandler(DTServerErrorException.class)
+    public ResponseEntity<ErrorResponse> handleServerException(DTServerErrorException exception) {
         log.warn("message: {}", exception.getMessage());
         return toResponse(exception.getHttpStatus(), exception.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ProblemDetail handleException(Exception exception) {
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
         log.error("exception: {}", exception);
-        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러");
+        return toResponse(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류");
     }
 
     private ResponseEntity<ErrorResponse> toResponse(HttpStatus status, String message) {
