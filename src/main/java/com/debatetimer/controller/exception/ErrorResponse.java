2a5a26824f2ad710f7b48af310b2f2dd6d95dd10
coli-geonwoo <coli-geonwoo@gmail.com>
2024-12-27 20:52:51 +0900

    style: 개행 수정

--- src/main/java/com/debatetimer/controller/exception/ErrorResponse.java
+++ src/main/java/com/debatetimer/controller/exception/ErrorResponse.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "예외 객체")
 public record ErrorResponse(
-
         @Schema(description = "사용자에게 보여줄 예외 메시지")
         @NotBlank
         String message
