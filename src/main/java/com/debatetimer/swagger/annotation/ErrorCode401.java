7a1d24dabdf6e6b577ee857d7a6e149858ff5b98
coli-geonwoo <coli-geonwoo@gmail.com>
2024-12-27 20:49:00 +0900

    style: 불필요한 import문 삭제

--- src/main/java/com/debatetimer/swagger/annotation/ErrorCode401.java
+++ src/main/java/com/debatetimer/swagger/annotation/ErrorCode401.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.core.annotation.AliasFor;
-import org.springframework.http.ProblemDetail;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
