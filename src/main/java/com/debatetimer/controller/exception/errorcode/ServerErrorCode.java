948f59c4d123d1e1261df8a56929df83c31b0407
coli-geonwoo <coli-geonwoo@gmail.com>
2024-12-27 20:13:43 +0900

    style: 개행 추가

--- src/main/java/com/debatetimer/controller/exception/errorcode/ServerErrorCode.java
+++ src/main/java/com/debatetimer/controller/exception/errorcode/ServerErrorCode.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ServerErrorCode implements ErrorCode {
+
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다. 관리자에게 문의하세요."),
     ;
 
