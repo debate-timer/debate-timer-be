113a75fcd002410a6d66c25f0fe2f4d091ede375
coli-geonwoo <coli-geonwoo@gmail.com>
2024-12-30 05:48:03 +0900

    test: 더 엄밀하게 테스트 변경

--- src/test/java/com/debatetimer/repository/parliamentary/ParliamentaryTimeBoxRepositoryTest.java
+++ src/test/java/com/debatetimer/repository/parliamentary/ParliamentaryTimeBoxRepositoryTest.java
@@ -3,6 +3,7 @@ package com.debatetimer.repository.parliamentary;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.debatetimer.BaseRepositoryTest;
+import com.debatetimer.domain.member.Member;
 import com.debatetimer.domain.parliamentary.ParliamentaryTable;
 import com.debatetimer.domain.parliamentary.ParliamentaryTimeBox;
 import java.util.List;
@@ -20,13 +21,18 @@ class ParliamentaryTimeBoxRepositoryTest extends BaseRepositoryTest {
 
         @Test
         void 특정_테이블의_타임박스를_모두_조회한다() {
-            ParliamentaryTable testTable = fixtureGenerator.generateParliamentaryTable();
-            ParliamentaryTimeBox firstBox = fixtureGenerator.generateParliamentaryTimeBox(testTable, 1);
-            ParliamentaryTimeBox secondBox = fixtureGenerator.generateParliamentaryTimeBox(testTable, 2);
+            Member chan = fixtureGenerator.generateMember("커찬");
+            Member bito = fixtureGenerator.generateMember("비토");
+            ParliamentaryTable chanTable = fixtureGenerator.generateParliamentaryTable(chan);
+            ParliamentaryTable bitoTable = fixtureGenerator.generateParliamentaryTable(bito);
+            ParliamentaryTimeBox chanBox1 = fixtureGenerator.generateParliamentaryTimeBox(chanTable, 1);
+            ParliamentaryTimeBox chanBox2 = fixtureGenerator.generateParliamentaryTimeBox(chanTable, 2);
+            ParliamentaryTimeBox bitoBox1 = fixtureGenerator.generateParliamentaryTimeBox(bitoTable, 2);
+            ParliamentaryTimeBox bitoBox2 = fixtureGenerator.generateParliamentaryTimeBox(bitoTable, 2);
 
-            List<ParliamentaryTimeBox> foundBoxes = parliamentaryTimeBoxRepository.findAllByParliamentaryTable(testTable);
+            List<ParliamentaryTimeBox> foundBoxes = parliamentaryTimeBoxRepository.findAllByParliamentaryTable(chanTable);
 
-            assertThat(foundBoxes).containsExactly(firstBox, secondBox);
+            assertThat(foundBoxes).containsExactly(chanBox1, chanBox2);
         }
     }
 }
