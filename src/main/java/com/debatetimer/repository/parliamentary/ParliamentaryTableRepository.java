6c94899a13bc9571a9e288a74dbfe9265f4dc9db
leegwichan <cnddkscndgus@naver.com>
2025-01-13 15:53:59 +0900

    refactor: ParliamentaryTableRepository 가 Repository 를 상속하도록 변경

--- src/main/java/com/debatetimer/repository/parliamentary/ParliamentaryTableRepository.java
+++ src/main/java/com/debatetimer/repository/parliamentary/ParliamentaryTableRepository.java
@@ -5,11 +5,18 @@ import com.debatetimer.domain.parliamentary.ParliamentaryTable;
 import com.debatetimer.exception.custom.DTClientErrorException;
 import com.debatetimer.exception.errorcode.ClientErrorCode;
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
 
-public interface ParliamentaryTableRepository extends JpaRepository<ParliamentaryTable, Long> {
+public interface ParliamentaryTableRepository extends Repository<ParliamentaryTable, Long> {
+
+    Optional<ParliamentaryTable> findById(long id);
 
     List<ParliamentaryTable> findAllByMember(Member member);
+
+    ParliamentaryTable save(ParliamentaryTable parliamentaryTable);
+
+    void delete(ParliamentaryTable table);
 
     default ParliamentaryTable getById(long tableId) {
         return findById(tableId)
