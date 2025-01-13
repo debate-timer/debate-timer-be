0cd71464c10aa1d28988c9eac876810c4faca598
leegwichan <cnddkscndgus@naver.com>
2025-01-13 15:50:33 +0900

    refactor: MemberRepository 가 Repository 를 상속하도록 변경

--- src/main/java/com/debatetimer/repository/member/MemberRepository.java
+++ src/main/java/com/debatetimer/repository/member/MemberRepository.java
@@ -3,9 +3,14 @@ package com.debatetimer.repository.member;
 import com.debatetimer.domain.member.Member;
 import com.debatetimer.exception.custom.DTClientErrorException;
 import com.debatetimer.exception.errorcode.ClientErrorCode;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends Repository<Member, Long> {
+
+    Optional<Member> findById(long id);
+
+    Member save(Member member);
 
     default Member getById(long id) {
         return findById(id)
