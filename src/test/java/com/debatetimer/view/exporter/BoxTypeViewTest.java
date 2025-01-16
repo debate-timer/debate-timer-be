c2828afdde7a43c5a9efad18ae4da35008745065
coli <kkwoo001021@naver.com>
2025-01-16 21:50:02 +0900

    test: view 테스트 수정

--- src/test/java/com/debatetimer/view/exporter/BoxTypeViewTest.java
+++ src/test/java/com/debatetimer/view/exporter/BoxTypeViewTest.java
@@ -1,16 +1,11 @@
 package com.debatetimer.view.exporter;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.debatetimer.domain.BoxType;
-import com.debatetimer.exception.custom.DTServerErrorException;
-import com.debatetimer.exception.errorcode.ServerErrorCode;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.mockito.Mockito;
 
 class BoxTypeViewTest {
 
@@ -18,24 +13,10 @@ class BoxTypeViewTest {
     class MapView {
 
         @ParameterizedTest
-        @EnumSource(value = BoxTypeView.class)
-        void 타임박스_타입과_일치하는_메시지를_반환한다(BoxTypeView boxTypeView) {
-            BoxType mappedBoxType = boxTypeView.getBoxType();
-            String expectedMessage = boxTypeView.getViewMessage();
-
-            String actualMappedMessage = BoxTypeView.mapView(mappedBoxType);
-
-            assertThat(actualMappedMessage)
-                    .isEqualTo(expectedMessage);
-        }
-
-        @Test
-        void 일치하는_타임박스_타입이_없을_경우_에러를_발생시킨다() {
-            BoxType mockBoxType = Mockito.mock(BoxType.class);
-
-            assertThatThrownBy(() -> BoxTypeView.mapView(mockBoxType))
-                    .isInstanceOf(DTServerErrorException.class)
-                    .hasMessage(ServerErrorCode.EXCEL_EXPORT_ERROR.getMessage());
+        @EnumSource(value = BoxType.class)
+        void 타임박스_타입과_일치하는_메시지를_반환한다(BoxType boxType) {
+            assertThatCode(() -> BoxTypeView.mapView(boxType))
+                    .doesNotThrowAnyException();
         }
     }
 }
