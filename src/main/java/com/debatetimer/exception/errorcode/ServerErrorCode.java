94d77f2f27c6c08f7689fec364f85d7b7dddd431
김건우 <kkwoo001021@naver.com>
2025-01-17 23:55:28 +0900

    [FEAT] Excel export 추가 기능 구현 (#49)

--- src/main/java/com/debatetimer/exception/errorcode/ServerErrorCode.java
+++ src/main/java/com/debatetimer/exception/errorcode/ServerErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ServerErrorCode implements ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다. 관리자에게 문의하세요."),
-    ;
+    EXCEL_EXPORT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "엑셀 변환 과정에서 오류가 발생하였습니다");
 
     private final HttpStatus status;
     private final String message;
