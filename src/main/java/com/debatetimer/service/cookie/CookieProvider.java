6a143f46180e5cbf9526ba640c700a6c485f9cdb
unifolio0 <boho82ha@gmail.com>
2025-01-29 16:58:17 +0900

    style: path 상수화

--- src/main/java/com/debatetimer/service/cookie/CookieProvider.java
+++ src/main/java/com/debatetimer/service/cookie/CookieProvider.java
@@ -6,17 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class CookieProvider {
 
+    private static final String PATH = "/";
+
     public Cookie createCookie(String cookieName, String token, long expirationMillis) {
         Cookie cookie = new Cookie(cookieName, token);
         cookie.setMaxAge((int) (expirationMillis / 1000));
-        cookie.setPath("/");
+        cookie.setPath(PATH);
         return cookie;
     }
 
     public Cookie deleteCookie(String cookieName) {
         Cookie cookie = new Cookie(cookieName, "");
         cookie.setMaxAge(0);
-        cookie.setPath("/");
+        cookie.setPath(PATH);
         return cookie;
     }
 }
