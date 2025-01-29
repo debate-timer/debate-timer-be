d2bcb66f3a25069cc86c17bd1a0f117e659ea888
unifolio0 <boho82ha@gmail.com>
2025-01-29 17:15:24 +0900

    refactor: AuthenticationConfig 추가

--- src/main/java/com/debatetimer/DebateTimerApplication.java
+++ src/main/java/com/debatetimer/DebateTimerApplication.java
@@ -1,13 +1,9 @@
 package com.debatetimer;
 
-import com.debatetimer.client.OAuthProperties;
-import com.debatetimer.service.auth.JwtTokenProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({OAuthProperties.class, JwtTokenProperties.class})
 public class DebateTimerApplication {
 
     public static void main(String[] args) {
