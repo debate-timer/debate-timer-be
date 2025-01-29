daa59081bdd079fe6ce75729bac7592e53c5dd26
unifolio0 <boho82ha@gmail.com>
2025-01-29 13:39:38 +0900

    test: 로그아웃 및 토큰 갱신 테스트

--- src/test/java/com/debatetimer/fixture/HeaderGenerator.java
+++ src/test/java/com/debatetimer/fixture/HeaderGenerator.java
@@ -3,6 +3,7 @@ package com.debatetimer.fixture;
 import com.debatetimer.domain.member.Member;
 import com.debatetimer.dto.member.MemberInfo;
 import com.debatetimer.service.auth.JwtTokenProvider;
+import com.debatetimer.service.cookie.CookieProvider;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import org.springframework.http.HttpHeaders;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Component;
 public class HeaderGenerator {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final CookieProvider cookieProvider;
 
-    public HeaderGenerator(JwtTokenProvider jwtTokenProvider) {
+    public HeaderGenerator(JwtTokenProvider jwtTokenProvider, CookieProvider cookieProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.cookieProvider = cookieProvider;
     }
 
     public Headers generateAccessTokenHeader(Member member) {
