36db32499aed9f306fc081045694f7085af3f17b
leegwichan <cnddkscndgus@naver.com>
2025-02-19 11:47:00 +0900

    refactor : 기본형 Column 에 `@NotNull` 제거

--- src/main/java/com/debatetimer/domain/DebateTimeBox.java
+++ src/main/java/com/debatetimer/domain/DebateTimeBox.java
@@ -15,7 +15,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class DebateTimeBox {
 
-    @NotNull
     private int sequence;
 
     @NotNull
