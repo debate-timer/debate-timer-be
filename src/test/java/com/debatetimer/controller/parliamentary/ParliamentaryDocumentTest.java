d28153049f566696caab92fa2f1b8f2e477cf8ef
leegwichan <cnddkscndgus@naver.com>
2025-02-21 11:20:44 +0900

    docs : API 명세서에 에러 코드 추가

--- src/test/java/com/debatetimer/controller/parliamentary/ParliamentaryDocumentTest.java
+++ src/test/java/com/debatetimer/controller/parliamentary/ParliamentaryDocumentTest.java
@@ -18,8 +18,8 @@ import com.debatetimer.controller.BaseDocumentTest;
 import com.debatetimer.controller.RestDocumentationRequest;
 import com.debatetimer.controller.RestDocumentationResponse;
 import com.debatetimer.controller.Tag;
-import com.debatetimer.domain.parliamentary.ParliamentaryBoxType;
 import com.debatetimer.domain.Stance;
+import com.debatetimer.domain.parliamentary.ParliamentaryBoxType;
 import com.debatetimer.dto.member.TableType;
 import com.debatetimer.dto.parliamentary.request.ParliamentaryTableCreateRequest;
 import com.debatetimer.dto.parliamentary.request.TableInfoCreateRequest;
@@ -115,8 +115,11 @@ public class ParliamentaryDocumentTest extends BaseDocumentTest {
                         "INVALID_TABLE_NAME_LENGTH",
                         "INVALID_TABLE_NAME_FORM",
                         "INVALID_TABLE_TIME",
+                        "INVALID_TIME_BOX_SEQUENCE",
+                        "INVALID_TIME_BOX_SPEAKER",
                         "INVALID_TIME_BOX_TIME",
-                        "INVALID_TIME_BOX_STANCE"
+                        "INVALID_TIME_BOX_STANCE",
+                        "INVALID_TIME_BOX_FORMAT"
                 }
         )
         @ParameterizedTest
@@ -175,7 +178,6 @@ public class ParliamentaryDocumentTest extends BaseDocumentTest {
 
         @Test
         void 의회식_테이블_조회_성공() {
-            long memberId = 4L;
             long tableId = 5L;
             ParliamentaryTableResponse response = new ParliamentaryTableResponse(
                     5L,
@@ -203,7 +205,6 @@ public class ParliamentaryDocumentTest extends BaseDocumentTest {
         @ParameterizedTest
         @EnumSource(value = ClientErrorCode.class, names = {"TABLE_NOT_FOUND", "NOT_TABLE_OWNER"})
         void 의회식_테이블_조회_실패(ClientErrorCode errorCode) {
-            long memberId = 4L;
             long tableId = 5L;
             doThrow(new DTClientErrorException(errorCode)).when(parliamentaryService).findTable(eq(tableId), any());
 
@@ -264,7 +265,6 @@ public class ParliamentaryDocumentTest extends BaseDocumentTest {
 
         @Test
         void 의회식_토론_테이블_수정() {
-            long memberId = 4L;
             long tableId = 5L;
             ParliamentaryTableCreateRequest request = new ParliamentaryTableCreateRequest(
                     new TableInfoCreateRequest("비토 테이블 2", "토론 주제 2", true, true),
@@ -303,14 +303,15 @@ public class ParliamentaryDocumentTest extends BaseDocumentTest {
                         "INVALID_TABLE_NAME_LENGTH",
                         "INVALID_TABLE_NAME_FORM",
                         "INVALID_TABLE_TIME",
+                        "INVALID_TIME_BOX_SEQUENCE",
+                        "INVALID_TIME_BOX_SPEAKER",
                         "INVALID_TIME_BOX_TIME",
                         "INVALID_TIME_BOX_STANCE",
-                        "NOT_TABLE_OWNER"
+                        "INVALID_TIME_BOX_FORMAT"
                 }
         )
         @ParameterizedTest
         void 의회식_테이블_생성_실패(ClientErrorCode errorCode) {
-            long memberId = 4L;
             long tableId = 5L;
             ParliamentaryTableCreateRequest request = new ParliamentaryTableCreateRequest(
                     new TableInfoCreateRequest("비토 테이블 2", "토론 주제 2", true, true),
@@ -352,7 +353,6 @@ public class ParliamentaryDocumentTest extends BaseDocumentTest {
 
         @Test
         void 의회식_테이블_삭제_성공() {
-            long memberId = 4L;
             long tableId = 5L;
             doNothing().when(parliamentaryService).deleteTable(eq(tableId), any());
 
@@ -370,7 +370,6 @@ public class ParliamentaryDocumentTest extends BaseDocumentTest {
         @EnumSource(value = ClientErrorCode.class, names = {"TABLE_NOT_FOUND", "NOT_TABLE_OWNER"})
         @ParameterizedTest
         void 의회식_테이블_삭제_실패(ClientErrorCode errorCode) {
-            long memberId = 4L;
             long tableId = 5L;
             doThrow(new DTClientErrorException(errorCode)).when(parliamentaryService).deleteTable(eq(tableId), any());
 
