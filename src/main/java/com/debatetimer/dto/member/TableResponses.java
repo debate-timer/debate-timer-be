a5b3f2037f85a497df2381a9acae6a7176649d73
leegwichan <cnddkscndgus@naver.com>
2025-03-04 14:37:10 +0900

    feat : 멤버의 토론 테이블 조회 시, 사용 역순으로 반환

--- src/main/java/com/debatetimer/dto/member/TableResponses.java
+++ src/main/java/com/debatetimer/dto/member/TableResponses.java
@@ -1,11 +1,17 @@
 package com.debatetimer.dto.member;
 
+import com.debatetimer.domain.DebateTable;
 import com.debatetimer.domain.parliamentary.ParliamentaryTable;
 import com.debatetimer.domain.timebased.TimeBasedTable;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
 public record TableResponses(List<TableResponse> tables) {
+
+    private static final Comparator<DebateTable> DEBATE_TABLE_COMPARATOR = Comparator
+            .comparing(DebateTable::getUsedAt)
+            .reversed();
 
     public TableResponses(List<ParliamentaryTable> parliamentaryTables,
                           List<TimeBasedTable> timeBasedTables) {
@@ -14,11 +20,9 @@ public record TableResponses(List<TableResponse> tables) {
 
     private static List<TableResponse> toTableResponses(List<ParliamentaryTable> parliamentaryTables,
                                                         List<TimeBasedTable> timeBasedTables) {
-        Stream<TableResponse> parliamentaryTableResponseStream = parliamentaryTables.stream()
-                .map(TableResponse::new);
-        Stream<TableResponse> timeBasedTableResponseStream = timeBasedTables.stream()
-                .map(TableResponse::new);
-        return Stream.concat(parliamentaryTableResponseStream, timeBasedTableResponseStream)
+        return Stream.concat(parliamentaryTables.stream(), timeBasedTables.stream())
+                .sorted(DEBATE_TABLE_COMPARATOR)
+                .map(TableResponse::new)
                 .toList();
     }
 }
