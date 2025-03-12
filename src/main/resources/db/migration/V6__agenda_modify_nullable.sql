354613bec5158f0cd113d836f3b59edade17a139
unifolio0 <boho82ha@gmail.com>
2025-03-12 09:23:23 +0900

    refactor: modify를 alter column으로 변경

--- src/main/resources/db/migration/V6__agenda_modify_nullable.sql
+++ src/main/resources/db/migration/V6__agenda_modify_nullable.sql
@@ -1,3 +1,3 @@
-alter table parliamentary_table modify agenda varchar(255) null;
-alter table time_based_table modify agenda varchar(255) null;
-alter table customize_table modify agenda varchar(255) null;
+alter table parliamentary_table alter column agenda set null;
+alter table time_based_table alter column agenda set null;
+alter table customize_table alter column agenda set null;
