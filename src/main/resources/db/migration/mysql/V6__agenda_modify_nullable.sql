c553fe17add14c6dbda76d1408cff1f0cec7e096
unifolio0 <boho82ha@gmail.com>
2025-03-12 09:41:59 +0900

    refactor: mysql, h2 분리 시도

---
+++ src/main/resources/db/migration/mysql/V6__agenda_modify_nullable.sql
@@ -0,0 +1,3 @@
+alter table parliamentary_table modify agenda varchar(255) null;
+alter table time_based_table modify agenda varchar(255) null;
+alter table customize_table modify agenda varchar(255) null;
