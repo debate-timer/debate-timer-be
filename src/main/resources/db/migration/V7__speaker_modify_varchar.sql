496bde405b9b5f4b4f8d8ec97ce9e2ff0d39abf2
coli <kkwoo001021@naver.com>
2025-03-13 00:43:42 +0900

    feat: flyway 변경 삭제

--- src/main/resources/db/migration/V7__speaker_modify_varchar.sql
+++
@@ -1,3 +0,0 @@
-alter table parliamentary_table modify speaker varchar (255) null;
-alter table time_based_table modify speaker varchar (255) null;
-alter table customize_table modify speaker varchar (255) null;
