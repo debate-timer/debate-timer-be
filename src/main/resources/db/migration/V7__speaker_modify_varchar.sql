dcc0945bdf2524c0fd88107f8abfe8e025180d8b
coli <kkwoo001021@naver.com>
2025-03-13 19:36:09 +0900

    feat: flyway로 스키마 변경

---
+++ src/main/resources/db/migration/V7__speaker_modify_varchar.sql
@@ -0,0 +1,3 @@
+alter table parliamentary_table modify speaker varchar (255) null;
+alter table time_based_table modify speaker varchar (255) null;
+alter table customize_table modify speaker varchar (255) null;
