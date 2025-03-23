97e577ef34dc0ab7fb72e45589f4e43fb9dd60ac
coli <kkwoo001021@naver.com>
2025-03-23 18:10:04 +0900

    test: channelNotifier 모킹

--- src/test/java/com/debatetimer/DatabaseSchemaManagerTest.java
+++ src/test/java/com/debatetimer/DatabaseSchemaManagerTest.java
@@ -2,11 +2,13 @@ package com.debatetimer;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import com.debatetimer.client.ChannelNotifier;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("flyway")
@@ -14,6 +16,9 @@ class DatabaseSchemaManagerTest {
 
     @Autowired
     private Flyway flyway;
+
+    @MockitoBean
+    private ChannelNotifier channelNotifier;
 
     @Test
     void contextLoads() {
--- src/test/java/com/debatetimer/DebateTimerApplicationTest.java
+++ src/test/java/com/debatetimer/DebateTimerApplicationTest.java
@@ -1,10 +1,15 @@
 package com.debatetimer;
 
+import com.debatetimer.client.ChannelNotifier;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class DebateTimerApplicationTest {
+
+    @MockitoBean
+    private ChannelNotifier channelNotifier;
 
     @Test
     void contextLoads() {
