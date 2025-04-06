c2a5c0e21682ae03697b6c47475177dc3e62495a
leegwichan <cnddkscndgus@naver.com>
2025-04-06 13:29:46 +0900

    feat : Spring Actuator 및 설정 추가

--- scripts/prod/replace-new-version.sh
+++ scripts/prod/replace-new-version.sh
@@ -19,4 +19,4 @@ fi
 
 JAR_FILE=$(ls /home/ubuntu/app/*.jar | head -n 1)
 
-sudo nohup java -Dspring.profiles.active=prod -Duser.timezone=Asia/Seoul -Dserver.port=8080 -jar "$JAR_FILE" &
+sudo nohup java -Dspring.profiles.active=prod,monitor -Duser.timezone=Asia/Seoul -Dserver.port=8080 -jar "$JAR_FILE" &
