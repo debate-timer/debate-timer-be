822669afd28183b607e7ddcde362b7d86355eb49
coli <kkwoo001021@naver.com>
2025-05-14 17:47:55 +0900

    refactor: 정적 팩터리 메서드로 변경

--- src/main/java/com/debatetimer/dto/member/TableResponses.java
+++ src/main/java/com/debatetimer/dto/member/TableResponses.java
@@ -1,7 +1,22 @@
 package com.debatetimer.dto.member;
 
+import com.debatetimer.domain.DebateTable;
+import com.debatetimer.domain.customize.CustomizeTable;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record TableResponses(List<TableResponse> tables) {
+
+    private static final Comparator<DebateTable> DEBATE_TABLE_COMPARATOR = Comparator
+            .comparing(DebateTable::getUsedAt)
+            .reversed();
+
+    public static TableResponses from(List<CustomizeTable> customizeTables) {
+        return customizeTables.stream()
+                .sorted(DEBATE_TABLE_COMPARATOR)
+                .map(TableResponse::new)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), TableResponses::new));
+    }
 
 }
--- src/main/java/com/debatetimer/service/member/MemberService.java
+++ src/main/java/com/debatetimer/service/member/MemberService.java
@@ -1,15 +1,13 @@
 package com.debatetimer.service.member;
 
-import com.debatetimer.domain.DebateTable;
+import com.debatetimer.domain.customize.CustomizeTable;
 import com.debatetimer.domain.member.Member;
 import com.debatetimer.dto.member.MemberCreateResponse;
 import com.debatetimer.dto.member.MemberInfo;
-import com.debatetimer.dto.member.TableResponse;
 import com.debatetimer.dto.member.TableResponses;
 import com.debatetimer.repository.customize.CustomizeTableRepository;
 import com.debatetimer.repository.member.MemberRepository;
-import java.util.Comparator;
-import java.util.stream.Collectors;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,20 +16,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
 
-    private static final Comparator<DebateTable> DEBATE_TABLE_COMPARATOR = Comparator
-            .comparing(DebateTable::getUsedAt)
-            .reversed();
-
     private final MemberRepository memberRepository;
     private final CustomizeTableRepository customizeTableRepository;
 
     @Transactional(readOnly = true)
     public TableResponses getTables(long memberId) {
         Member member = memberRepository.getById(memberId);
-        return customizeTableRepository.findAllByMember(member).stream()
-                .sorted(DEBATE_TABLE_COMPARATOR)
-                .map(TableResponse::new)
-                .collect(Collectors.collectingAndThen(Collectors.toList(), TableResponses::new));
+        List<CustomizeTable> memberTables = customizeTableRepository.findAllByMember(member);
+        return TableResponses.from(memberTables);
     }
 
     @Transactional
