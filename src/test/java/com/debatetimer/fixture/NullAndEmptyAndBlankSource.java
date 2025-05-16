94bf46891f5416b73363d025c081450344284b6e
unifolio0 <boho82ha@gmail.com>
2025-05-16 16:41:00 +0900

    refactor: 불필요한 타겟 제거

--- src/test/java/com/debatetimer/fixture/NullAndEmptyAndBlankSource.java
+++ src/test/java/com/debatetimer/fixture/NullAndEmptyAndBlankSource.java
@@ -5,13 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@NullSource
-@EmptySource
 @ArgumentsSource(BlankArgumentsProvider.class)
+@NullAndEmptySource
 public @interface NullAndEmptyAndBlankSource {
 }
