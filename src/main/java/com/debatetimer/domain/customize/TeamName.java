e690c6def0cf9cf18c058b90a1b2f824def20421
coli <kkwoo001021@naver.com>
2025-05-28 02:18:48 +0900

    feat: 팀 이름 도메인 분리

---
+++ src/main/java/com/debatetimer/domain/customize/TeamName.java
@@ -0,0 +1,26 @@
+package com.debatetimer.domain.customize;
+
+import com.debatetimer.exception.custom.DTClientErrorException;
+import com.debatetimer.exception.errorcode.ClientErrorCode;
+
+public class TeamName {
+
+    private static final String NAME_REGEX = "^[\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\s]+$";
+    public static final int NAME_MAX_LENGTH = 8;
+
+    private final String value;
+
+    public TeamName(String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(String teamName) {
+        if (teamName.length() > NAME_MAX_LENGTH) {
+            throw new DTClientErrorException(ClientErrorCode.INVALID_TEAM_NAME_LENGTH);
+        }
+        if (!teamName.matches(NAME_REGEX)) {
+            throw new DTClientErrorException(ClientErrorCode.INVALID_TEAM_NAME_FORM);
+        }
+    }
+}
