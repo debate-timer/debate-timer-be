ffc3ad3778158ae589a0f11e95292595b9a10250
coli <kkwoo001021@naver.com>
2025-07-10 18:30:37 +0900

    refactor: 일관성에 맞도록 타입 변경

--- src/main/resources/db/migration/V9__create_poll_and_vote_table.sql
+++ src/main/resources/db/migration/V9__create_poll_and_vote_table.sql
@@ -2,19 +2,19 @@ CREATE TABLE poll
 (
     id             BIGINT AUTO_INCREMENT PRIMARY KEY,
     table_id       BIGINT       NOT NULL,
-    status      VARCHAR(255) NOT NULL,
+    status      ENUM ('PROGRESS','DONE') NOT NULL,
     pros_team_name VARCHAR(255) NOT NULL,
     cons_team_name VARCHAR(255) NOT NULL,
     agenda      VARCHAR(255) NOT NULL,
-    created_at     DATETIME(6) NOT NULL,
-    modified_at DATETIME(6) NOT NULL
+    created_at  TIMESTAMP    NOT NULL,
+    modified_at TIMESTAMP    NOT NULL
 );
 
 CREATE TABLE vote
 (
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
     poll_id          BIGINT       NOT NULL,
-    team             VARCHAR(255) NOT NULL,
+    team ENUM ('CONS','PROS') NOT NULL,
     name             VARCHAR(255) NOT NULL,
     participant_code VARCHAR(255) NOT NULL
 );
