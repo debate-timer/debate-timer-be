4c3baf532d746c96ccd0ce70b94b10fa789321ba
김건우 <kkwoo001021@naver.com>
2025-07-11 00:30:04 +0900

    [FEAT] 투표 API 기초 엔티티 세팅 (#196)

---
+++ src/main/java/com/debatetimer/entity/poll/VoteEntity.java
@@ -0,0 +1,42 @@
+package com.debatetimer.entity.poll;
+
+import com.debatetimer.domain.poll.VoteTeam;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "vote")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class VoteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "poll_id")
+    private PollEntity poll;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private VoteTeam team;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String participantCode;
+}
