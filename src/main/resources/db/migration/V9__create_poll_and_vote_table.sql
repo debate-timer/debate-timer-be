4842abdab6d6ab11c7661b46ad99969decfc8126
coli <kkwoo001021@naver.com>
2025-07-10 09:57:07 +0900

    refactor: flyway 파일 스크립트 변경

--- src/main/resources/db/migration/V9__create_poll_and_vote_table.sql
+++ src/main/resources/db/migration/V9__create_poll_and_vote_table.sql
@@ -2,20 +2,24 @@ CREATE TABLE poll
 (
     id             BIGINT AUTO_INCREMENT PRIMARY KEY,
     table_id       BIGINT       NOT NULL,
-    status VARCHAR(255) NOT NULL,
+    status      VARCHAR(255) NOT NULL,
     pros_team_name VARCHAR(255) NOT NULL,
     cons_team_name VARCHAR(255) NOT NULL,
-    agenda         TEXT,
+    agenda      VARCHAR(255) NOT NULL,
     created_at     DATETIME(6) NOT NULL,
-    updated_at     DATETIME(6) NOT NULL
+    modified_at DATETIME(6) NOT NULL
 );
 
 CREATE TABLE vote
 (
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
     poll_id          BIGINT       NOT NULL,
-    team VARCHAR(255) NOT NULL,
+    team             VARCHAR(255) NOT NULL,
     name             VARCHAR(255) NOT NULL,
-    participant_code VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_vote_poll FOREIGN KEY (poll_id) REFERENCES poll (id) ON DELETE CASCADE
+    participant_code VARCHAR(255) NOT NULL
 );
+
+alter table vote
+    add constraint vote_to_poll
+        foreign key (poll_id)
+            references poll (id);
