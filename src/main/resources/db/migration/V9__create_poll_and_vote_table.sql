805f0be7ac528c8930ffee1808bc7aa09ff4a161
coli <kkwoo001021@naver.com>
2025-07-10 09:49:36 +0900

    chore: flyway 파일 이름 변경

--- src/main/resources/db/migration/V9__create_poll_and_vote_table.sql
+++ src/main/resources/db/migration/V9__create_poll_and_vote_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE poll
 (
     id             BIGINT AUTO_INCREMENT PRIMARY KEY,
     table_id       BIGINT       NOT NULL,
-    status         VARCHAR(255) NOT NULL, -- ENUM형은 VARCHAR로 매핑됨
+    status VARCHAR(255) NOT NULL,
     pros_team_name VARCHAR(255) NOT NULL,
     cons_team_name VARCHAR(255) NOT NULL,
     agenda         TEXT,
@@ -14,8 +14,8 @@ CREATE TABLE vote
 (
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
     poll_id          BIGINT       NOT NULL,
-    team             VARCHAR(255) NOT NULL, -- ENUM형은 VARCHAR로 매핑됨
+    team VARCHAR(255) NOT NULL,
     name             VARCHAR(255) NOT NULL,
     participant_code VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_vote_poll FOREIGN KEY (poll_id) REFERENCES poll_entity (id) ON DELETE CASCADE
+    CONSTRAINT fk_vote_poll FOREIGN KEY (poll_id) REFERENCES poll (id) ON DELETE CASCADE
 );
