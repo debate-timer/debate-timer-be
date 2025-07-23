9c0bf916f912fbde7bc20d8e4836b4234af18260
coli <kkwoo001021@naver.com>
2025-07-24 01:47:08 +0900

    feat: VoteInfo 계층 구현

--- src/main/java/com/debatetimer/domain/poll/Poll.java
+++ src/main/java/com/debatetimer/domain/poll/Poll.java
@@ -15,8 +15,8 @@ public class Poll {
     private final TeamName consTeamName;
     private final Agenda agenda;
 
-    public Poll(Long id, long tableId, long userId, PollStatus status, String prosTeamName, String consTeamName,
-                String agenda) {
+    public Poll(Long id, long tableId, long userId, PollStatus status,
+                String prosTeamName, String consTeamName, String agenda) {
         this(id, tableId, userId, status, new TeamName(prosTeamName), new TeamName(consTeamName), new Agenda(agenda));
     }
 }
---
+++ src/main/java/com/debatetimer/domain/poll/VoteInfo.java
@@ -0,0 +1,16 @@
+package com.debatetimer.domain.poll;
+
+public class VoteInfo {
+
+    private final long pollId;
+    private final long totalCount;
+    private final long prosCount;
+    private final long consCount;
+
+    public VoteInfo(long pollId, long prosCount, long consCount) {
+        this.pollId = pollId;
+        this.totalCount = prosCount + consCount;
+        this.prosCount = prosCount;
+        this.consCount = consCount;
+    }
+}
