c9666f9cdbce234ba772a1a82785889fd5ad4c41
coli <kkwoo001021@naver.com>
2025-07-24 18:19:51 +0900

    style: 무의미한 개행 제거

--- src/main/java/com/debatetimer/exception/errorcode/ClientErrorCode.java
+++ src/main/java/com/debatetimer/exception/errorcode/ClientErrorCode.java
@@ -51,7 +51,6 @@ public enum ClientErrorCode implements ResponseErrorCode {
     INVALID_POLL_PARTICIPANT_NAME(HttpStatus.BAD_REQUEST, "잘못된 투표자 이름입니다"),
     INVALID_POLL_PARTICIPANT_CODE(HttpStatus.BAD_REQUEST, "잘못된 투표참여 코드입니다"),
 
-
     TABLE_NOT_FOUND(HttpStatus.NOT_FOUND, "토론 테이블을 찾을 수 없습니다."),
     NOT_TABLE_OWNER(HttpStatus.UNAUTHORIZED, "테이블을 소유한 회원이 아닙니다."),
     POLL_NOT_FOUND(HttpStatus.NOT_FOUND, "투표를 찾을 수 없습니다."),
