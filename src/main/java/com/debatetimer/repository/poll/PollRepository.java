32cfd076ce4be34e19bd59746547fc7191b98b90
coli <kkwoo001021@naver.com>
2025-07-24 18:20:24 +0900

    style: 무의미한 개행 제거

--- src/main/java/com/debatetimer/repository/poll/PollRepository.java
+++ src/main/java/com/debatetimer/repository/poll/PollRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PollRepository extends JpaRepository<PollEntity, Long> {
 
     Optional<PollEntity> findByIdAndMemberId(long id, long memberId);
-
 }
