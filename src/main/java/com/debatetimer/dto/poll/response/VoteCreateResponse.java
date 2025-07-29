2ae97ca793e8debf3a4bc155bf678f6a6f9723ac
coli <kkwoo001021@naver.com>
2025-07-29 09:38:40 +0900

    refactor: 불필요한 validation 제거

--- src/main/java/com/debatetimer/dto/poll/response/VoteCreateResponse.java
+++ src/main/java/com/debatetimer/dto/poll/response/VoteCreateResponse.java
@@ -2,14 +2,12 @@ package com.debatetimer.dto.poll.response;
 
 import com.debatetimer.domain.poll.Vote;
 import com.debatetimer.domain.poll.VoteTeam;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record VoteCreateResponse(
         long id,
-        @NotBlank String name,
-        @NotBlank String participateCode,
-        @NotNull VoteTeam team
+        String name,
+        String participateCode,
+        VoteTeam team
 ) {
 
     public VoteCreateResponse(Vote vote) {
