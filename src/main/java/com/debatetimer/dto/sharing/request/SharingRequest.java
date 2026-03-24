c955a7fd67ea0aa27bac2dac2a9d4665ba44a642
coli <kkwoo001021@naver.com>
2026-03-25 00:47:37 +0900

    feat: Optional을 사용하는 방식으로 변경

--- src/main/java/com/debatetimer/dto/sharing/request/SharingRequest.java
+++ src/main/java/com/debatetimer/dto/sharing/request/SharingRequest.java
@@ -2,17 +2,20 @@ package com.debatetimer.dto.sharing.request;
 
 import com.debatetimer.domain.sharing.TimerEventInfo;
 import com.debatetimer.domain.sharing.TimerEventType;
+import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
+import java.util.Optional;
 
 public record SharingRequest(
         TimerEventType eventType,
-        @Valid TimerEventInfoRequest data
+
+        @Valid
+        @Nullable
+        TimerEventInfoRequest data
 ) {
 
-    public TimerEventInfo toTimerEventInfo() {
-        if (data == null) {
-            return null;
-        }
-        return data.toTimerEventInfo();
+    public Optional<TimerEventInfo> toTimerEventInfo() {
+        return Optional.ofNullable(data)
+                .map(TimerEventInfoRequest::toTimerEventInfo);
     }
 }
--- src/main/java/com/debatetimer/dto/sharing/response/SharingResponse.java
+++ src/main/java/com/debatetimer/dto/sharing/response/SharingResponse.java
@@ -2,17 +2,25 @@ package com.debatetimer.dto.sharing.response;
 
 import com.debatetimer.domain.sharing.TimerEventInfo;
 import com.debatetimer.domain.sharing.TimerEventType;
+import jakarta.annotation.Nullable;
+import java.util.Optional;
 
 public record SharingResponse(
         TimerEventType eventType,
+
+        @Nullable
         TimerEventInfoResponse data
 ) {
 
-    public SharingResponse(TimerEventType eventType, TimerEventInfo timerEventInfo) {
+    public SharingResponse(
+            TimerEventType eventType,
+            Optional<TimerEventInfo> timerEventInfo
+    ) {
         this(
                 eventType,
-                new TimerEventInfoResponse(timerEventInfo)
+                timerEventInfo
+                        .map(TimerEventInfoResponse::new)
+                        .orElse(null)
         );
     }
-
 }
