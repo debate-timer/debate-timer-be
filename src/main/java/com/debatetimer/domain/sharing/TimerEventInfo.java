e82a65efa5e63561c3255f44958e91b6e71109fd
coli <kkwoo001021@naver.com>
2026-03-25 00:29:18 +0900

    feat: nullable 표시

--- src/main/java/com/debatetimer/domain/sharing/TimerEventInfo.java
+++ src/main/java/com/debatetimer/domain/sharing/TimerEventInfo.java
@@ -2,6 +2,8 @@ package com.debatetimer.domain.sharing;
 
 import com.debatetimer.domain.customize.CustomizeBoxType;
 import com.debatetimer.domain.customize.Stance;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,8 +11,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TimerEventInfo {
 
+    @NotNull
     private final CustomizeBoxType timerType;
+
     private final int sequence;
+
+    @Nullable
     private final Stance currentTeam;
+
     private final long remainingTime;
 }
