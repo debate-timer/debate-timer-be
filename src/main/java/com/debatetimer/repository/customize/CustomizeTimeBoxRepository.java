2fee23e802f6f6dad5e1175a75560f676e4b26cb
coli <kkwoo001021@naver.com>
2026-04-28 12:17:34 +0900

    feat: coalesce를 통한 null 방어로직 추가

--- src/main/java/com/debatetimer/repository/customize/CustomizeTimeBoxRepository.java
+++ src/main/java/com/debatetimer/repository/customize/CustomizeTimeBoxRepository.java
@@ -13,7 +13,7 @@ public interface CustomizeTimeBoxRepository extends Repository<CustomizeTimeBoxE
 
     List<CustomizeTimeBoxEntity> findAllByCustomizeTable(CustomizeTableEntity table);
 
-    @Query("SELECT SUM(ctb.time) FROM CustomizeTimeBoxEntity ctb WHERE ctb.customizeTable.id = :tableId")
+    @Query("SELECT COALESCE(SUM(ctb.time), 0)  FROM CustomizeTimeBoxEntity ctb WHERE ctb.customizeTable.id = :tableId")
     long sumTimeByTableId(long tableId);
 
     @Query("DELETE FROM CustomizeTimeBoxEntity ctb WHERE ctb.customizeTable.id = :tableId")
