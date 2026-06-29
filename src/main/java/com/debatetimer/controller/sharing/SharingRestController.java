4c779280b387fa3e0afc0d06cf2f1be5907daf29
coli <kkwoo001021@naver.com>
2026-06-29 18:47:14 +0900

    feat: 비회원용 조회메서드 findTable 구현

--- src/main/java/com/debatetimer/controller/sharing/SharingRestController.java
+++ src/main/java/com/debatetimer/controller/sharing/SharingRestController.java
@@ -3,11 +3,14 @@ package com.debatetimer.controller.sharing;
 import com.debatetimer.controller.auth.AuthMember;
 import com.debatetimer.controller.tool.jwt.AuthManager;
 import com.debatetimer.domain.member.Member;
+import com.debatetimer.dto.customize.response.CustomizeTableResponse;
 import com.debatetimer.dto.sharing.response.ChairmanTokenResponse;
 import com.debatetimer.service.customize.CustomizeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +28,13 @@ public class SharingRestController {
         long debateTime = customizeService.findDebateTime(tableId, member);
         String chairmanToken = authManager.issueChairmanToken(member, debateTime * 2);
         return new ChairmanTokenResponse(chairmanToken);
+    }
+
+    @GetMapping("/api/live/table/customize/{tableId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CustomizeTableResponse getTable(
+            @PathVariable long tableId
+    ) {
+        return customizeService.findTable(tableId);
     }
 }
